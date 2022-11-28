87e20eb618654530b2a0bacacb5a05769a4319bc
Sma1lboy <541898146chen@gmail.com>
2022-11-28 14:11:49 -0500

    scan refactory again

---
+++ grpc/src/cpp/src/cmake/build/CMakeFiles/greeter_client.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/greeter_client.dir/greeter_client.cc.o"
+  "CMakeFiles/greeter_client.dir/greeter_client.cc.o.d"
+  "greeter_client"
+  "greeter_client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/greeter_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
