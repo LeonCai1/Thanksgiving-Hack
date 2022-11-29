f540688599d329e9e5e2d9cf7580431c33ed9f49
Sma1lboy <541898146chen@gmail.com>
2022-11-29 08:00:10 -0500

    keep optimazation hierarchy 2.0

---
+++ grpc/cmake/build/CMakeFiles/CMakeScratch/TryCompile-MlUI3S/CMakeFiles/cmTC_64e65.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/jacksonchen/Documents/Github/temp/Thanksgiving-Hack/grpc/cmake/build/CMakeFiles/CMakeScratch/TryCompile-MlUI3S/HAVE_SYS_UIO_H.c" "CMakeFiles/cmTC_64e65.dir/HAVE_SYS_UIO_H.c.o" "gcc" "CMakeFiles/cmTC_64e65.dir/HAVE_SYS_UIO_H.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ grpc/cmake/build/CMakeFiles/CMakeScratch/TryCompile-MlUI3S/CMakeFiles/cmTC_64e65.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cmTC_64e65.
+# This may be replaced when dependencies are built.
